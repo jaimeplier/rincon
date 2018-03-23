f5adf6fca8fa1961c63ecafc480bb0e26eb86ab9
Jaime <jaimefh92@gmail.com>
2018-03-23 13:18:21 -0600

    add components and basic paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MdlModule } from "@angular-mdl/core";
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ViewAuctionsComponent } from './view-auctions/view-auctions.component';
 import { ItemAuctionComponent } from './item-auction/item-auction.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ItemViewComponent } from './item-view/item-view.component';
+import { ProfileItemsComponent } from './profile-items/profile-items.component';
 
+const appRoutes:Routes = [
+  { path: '', component: ItemAuctionComponent  },
+  { path: 'perfil', component: ProfileComponent  },
+  { path: 'mi-chachara', component: ProfileItemsComponent  },
+  { path: ':id', component: ItemViewComponent  },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewAuctionsComponent,
-    ItemAuctionComponent
+    ItemAuctionComponent,
+    ProfileComponent,
+    ItemViewComponent,
+    ProfileItemsComponent,
   ],
   imports: [
     BrowserModule,
-    MdlModule
+    MdlModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/profile-items/profile-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-items',
+  templateUrl: './profile-items.component.html',
+  styleUrls: ['./profile-items.component.css']
+})
+export class ProfileItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

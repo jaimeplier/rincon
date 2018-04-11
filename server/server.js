cfc89d58cef676523436d518433a012ef7b11ce9
jaimeplier <jaimefh92@gmail.com>
2018-04-11 18:14:18 -0500

    fixed route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 }); */
 
 app.get('*', function(req, res, next) {
-  res.sendFile(__dirname+"../dist/index.html");
+  res.sendFile(__dirname+"/../dist/index.html");
 });
 
 
